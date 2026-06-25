483ae95e6c518d50deb10113caed003d1411400c
timmysjit <inwookk0710@gmail.com>
2026-06-25 13:48:31 +0800

    refactor code with config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+DELTA_X_LEAK = 20
+NO_LEAK_PORTION = 0.1
+
+TRAIN = {
+    "demand_range" : [2, 2, 0.05],
+    "noise_amplitude" : 0.05}
+
+
+TL = {
+    "num_samples" : 1,
+    "leak_demand_min" : 2,
+    "leak_demand_max" : 2,
+    "noise_amplitutde" : 0.05
+}
+
+TEST = {
+    "num_samples" : 1,
+    "leak_demand_min" : 2,
+    "leak_demand_max" : 2,
+    "noise_amplitutde" : 0.05
+}
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from generator import *
 from model_training import *
 import time
-
+from config import *
 def main():
     start_time = time.perf_counter()
     file = open("geojson.json", "r")
@@ -10,12 +10,7 @@ def main():
     file.close()
 
     hexagon_geojson = json.loads(data["hexagonGeoJson"])
-    delta_x_leak = 20
-    demand_range = [2, 2, 0.05]
-    no_leak_portion = 0
-    noise_amplitude = 0.05
     network_inp_content = data["inpContent"]
-    
     crs = data["crsCode"]
     hexagon_radius = data["hexagonRadius"]
     step3_sensor_attachments = data["step3SelectedNodeIds"]
@@ -24,12 +19,12 @@ def main():
     hexagon_geojson = hexagon_geojson,
     network_inp_content = network_inp_content,
     crs = crs,
-    delta_x_leak = delta_x_leak,
+    delta_x_leak = DELTA_X_LEAK,
     hexagon_radius = hexagon_radius,
     step3_sensor_attachments = step3_sensor_attachments,
-    leak_demand_range = demand_range,
-    no_leak_portion = no_leak_portion,
-    noise_amplitude = noise_amplitude,
+    leak_demand_range = TRAIN["demand_range"],
+    no_leak_portion = NO_LEAK_PORTION,
+    noise_amplitude = TRAIN["noise_amplitude"],
     ) 
 
     epoch_count = 1
@@ -37,46 +32,35 @@ def main():
 
     gcn_model_id = train_output["modelId"]
 
-
-    tl_num_samples = 1
-    tl_leak_demand_min = 2
-    tl_leak_demand_max = 2
-    tl_noise_amplitude = 0.05
-
     tl_data = generate_random_test_data(
     hexagon_geojson = hexagon_geojson,
     network_inp_content = network_inp_content,
     crs = crs,
-    delta_x_leak = delta_x_leak,
+    delta_x_leak = DELTA_X_LEAK,
     hexagon_radius = hexagon_radius,
     step3_sensor_attachments = step3_sensor_attachments,
-    num_samples = tl_num_samples,
-    leak_demand_min = tl_leak_demand_min,
-    leak_demand_max = tl_leak_demand_max,
-    noise_amplitude = tl_noise_amplitude,
-    no_leak_portion = no_leak_portion,
+    num_samples = TL["num_samples"],
+    leak_demand_min = TL["leak_demand_min"],
+    leak_demand_max = TL["leak_demand_max"],
+    noise_amplitude = TL["noise_amplitude"],
+    no_leak_portion = NO_LEAK_PORTION,
     )
 
     tl_output = transfer_learning(model_id=gcn_model_id, dataset_payload=tl_data, network_inp_content=network_inp_content, epoch_count=epoch_count)
     tl_model_id = tl_output["modelId"]
 
-    test_num_samples = 1
-    test_leak_demand_min = 2
-    test_leak_demand_max = 2
-    test_noise_amplitude = 0.05
-
     test_dataset = generate_random_test_data(
         hexagon_geojson=hexagon_geojson,
         network_inp_content=network_inp_content,
         crs=crs,
-        delta_x_leak=delta_x_leak,
+        delta_x_leak=DELTA_X_LEAK,
         hexagon_radius=hexagon_radius,
         step3_sensor_attachments=step3_sensor_attachments,
-        num_samples=test_num_samples,
-        leak_demand_min=test_leak_demand_min,
-        leak_demand_max=test_leak_demand_max,
-        noise_amplitude=test_noise_amplitude,
-        no_leak_portion=no_leak_portion,
+        num_samples=TEST["num_samples"],
+        leak_demand_min=TEST["leak_demand_min"],
+        leak_demand_max=TEST["leak_demand_max"],
+        noise_amplitude=TEST["noise_amplitude"],
+        no_leak_portion=NO_LEAK_PORTION,
     )
 
     prediction = predict_leakage(model_id=tl_model_id, test_dataset=test_dataset["dataset"], network_inp_content=network_inp_content)
